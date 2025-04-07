d2e15544ade6ff36ff8d885d586763fbe49169d2
gncnWkd <hyunchan7764@naver.com>
2025-04-08 00:06:23 +0900

    parameters, DB save from input file

--- 2020014975.py
+++ 2020014975.py
@@ -1,1 +1,20 @@
 import sys
+
+minSupport = sys.argv[1]
+inputFileName = sys.argv[2]
+outputFileName = sys.argv[3]
+
+inputFile = open(inputFileName, 'r')
+outputFile = open(outputFileName, 'w')
+
+DB = []
+
+while True: 
+    line = inputFile.readline()
+    if not line:
+        break
+    line = line.rstrip("\n")
+    DB.append(line.split("\t"))
+    
+
+
